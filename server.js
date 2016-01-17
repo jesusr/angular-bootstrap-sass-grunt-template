93feefa56dc2bedb06d48a3dde75640440f3ded6
Jesús R Peinado <jesusrpeinado@gmail.com>
2016-01-17 23:20:11 +0100

    Updating sendFile function

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
+var express = require('express'),
+    app = express(),
+    bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({
     extended: true
@@ -8,7 +8,6 @@ app.use(bodyParser.urlencoded({
 app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/dist'));
-console.log(__dirname + '/dist');
 var port = process.env.PORT || 4443;
 
 var api = require('./api/api.router.js');
@@ -16,8 +15,7 @@ app.use('/api', api);
 app.use('/api/*', api);
 
 app.get('*', function(req, res) {
-    res.sendfile('./dist/index.html');
+    res.sendFile(__dirname + '/dist/index.html');
 });
 
 app.listen(port);
-console.log('Listening at ' + port);
